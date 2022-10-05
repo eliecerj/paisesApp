5bf5123175a3e9c013788cafa94dedea0f1b1b41
Eliecer Arteaga <earteaga@sura.cl>
2022-10-05 18:18:31 -0300

    agrego servicio busqueda pais, tipado pais

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-country',
   templateUrl: './by-country.component.html',
   styleUrls: ['./by-country.component.css']
 })
-export class ByCountryComponent implements OnInit {
+export class ByCountryComponent  {
 
-  constructor() { }
+  termino: string = '';
+  isError: boolean = false;
 
-  ngOnInit(): void {
+  constructor( private countryService: CountryService) { }
+
+  buscar() {
+    this.isError = false;
+    console.log(this.termino);
+
+    this.countryService.searchCountry(this.termino)
+      .subscribe( (countries) => {
+        console.log(countries);
+        
+
+
+      }, (err)=> {
+        console.info('Error', err);
+        this.isError = true;
+      })
+    
   }
+
 
 }
